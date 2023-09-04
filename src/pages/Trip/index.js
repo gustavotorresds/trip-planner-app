dd1d7c558fb2f1837c34eca66e0a43a238d07501
Gustavo Torres <gustavotorresds@gmail.com>
2023-09-04 14:31:06 -0700

    Build simple UI for trip query and itinerary

--- src/pages/Trip/index.js
+++ src/pages/Trip/index.js
@@ -1,10 +1,73 @@
 import '../../assets/app.css';
-import { tripDateString } from '../../utils';
+import {
+  ItineraryType,
+  tripDateString,
+  destinationCitiesString,
+  numberOfPeopleString,
+  dayItineraryDate,
+  itineraryItemSummaryDescription,
+} from '../../utils';
+
+const trip = {
+      title: 'A week with family in Brazil',
+      startDate: '2023-08-20',
+      endDate: '2023-11-20',
+      cityFrom: 'San Francisco',
+      destinationCities: [
+          { city: 'São Paulo', numberOfDays: 4 },
+          { city: 'Rio de Janeiro', numberOfDays: 3 }
+      ],
+      numberOfPeople: 1,
+      itinerary: [
+        [
+          {
+            itineraryType: ItineraryType['Flight'],
+            description: 'GRU to MIA with United Airlines'
+          },
+        ],
+        [
+          {
+            itineraryType: ItineraryType['LodgingCheckin'],
+            description: 'Residence Inn'
+          },
+          {
+            itineraryType: ItineraryType['Activity'],
+            description: 'Sunset at the bay'
+          },
+        ],
+      ],
+    };
 
 function Trip() {
   return (
-      <div className="page trip">
-        <h1>TRIP</h1>
+      <div className="trip">
+        <div className="header">
+        A week with family in Brazil
+        </div>
+
+        <div className="page">
+          <div className="query-summary">
+            {destinationCitiesString(trip)} • {tripDateString(trip)} • {numberOfPeopleString(trip)}
+          </div>
+
+          <div className="overall-itinerary">
+            {trip.itinerary.map((dayItinerary, idx) =>
+              <div className="day-itinerary" key={`day-${idx}`}>
+                <div className="date">
+                {dayItineraryDate(trip, idx)}
+                </div>
+
+                <div className="day-itinerary-items">
+                  {dayItinerary.map((itineraryItem, idx2) =>
+                    <div className="day-itinerary-item" key={`day-${idx}-item-${idx2}`}>
+                      {itineraryItemSummaryDescription(itineraryItem)}
+                    </div>
+                  )}
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
   );
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,12 @@
 // TODO: bug on the date displayed, off by one probably due to timezone
 
+export const ItineraryType = {
+  Activity: 0,
+  Flight: 1,
+  LodgingCheckin: 2,
+  LodgingCheckout: 3,
+}
+
 export const tripDateString = ({ startDate, endDate }) => {
   const startDateJS = new Date(startDate);
   const endDateJS = new Date(endDate);
@@ -31,4 +38,36 @@ export const destinationCitiesString = ({ destinationCities }) => {
 
 export const cityFromString = ({ cityFrom }) => {
   return cityFrom;
+}
+
+export const dayItineraryDate = ({ startDate, endDate, itinerary,}, idx) => {
+  const itineraryDateJS = new Date(startDate);
+  itineraryDateJS.setDate(itineraryDateJS.getDate() + idx);
+  // const endDateJS = new Date(endDate); // TODO: use it in edge case scenarios
+
+  const itineraryDay = itineraryDateJS.getDate();
+  const itineraryMonth = itineraryDateJS.toLocaleString('default', { month: 'short' });
+  const itineraryYear = itineraryDateJS.getFullYear();
+
+  return `${itineraryMonth} ${itineraryDay}`
+}
+
+export const itineraryItemSummaryDescription = ({ itineraryType, description}) => {
+  let type = 'TBD';
+
+  switch (itineraryType) {
+    case ItineraryType['Activity']:
+      type = 'Activity';
+      break;
+    case ItineraryType['Flight']:
+      type = 'Flight';
+      break;
+    case ItineraryType['LodgingCheckin']:
+      type = 'Check-in';
+      break;
+    case ItineraryType['LodgingCheckout']:
+      type = 'Check-out';
+  }
+
+  return `${type}: ${description}`
 }
