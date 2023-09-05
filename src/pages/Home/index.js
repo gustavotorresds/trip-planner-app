1b3d7cacdadd24cc4482f7e788ed796515bbf4c0
Gustavo Torres <gustavotorresds@gmail.com>
2023-09-04 20:37:18 -0700

    Trip (#3)
    * Build simple UI for trip query and itinerary
    
    * Implement context to pass trip data and links for the right trip page to
    open
    
    * A few small updates

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,34 +1,12 @@
 import { Link } from 'react-router-dom';
 
 import '../../assets/app.css';
-import { tripDateString } from '../../utils';
-
-const trips = [
-    {
-      title: 'A week with family in the U.S.',
-      startDate: '2023-08-20',
-      endDate: '2023-08-27',
-      cityFrom: 'São Paulo',
-      destinationCities: [
-          { city: 'Miami', numberOfDays: 4 },
-          { city: 'San Francisco', numberOfDays: 3 }
-      ],
-      numberOfPeople: 4,
-    },
-    {
-      title: 'A week with family in Brazil',
-      startDate: '2023-08-20',
-      endDate: '2023-11-20',
-      cityFrom: 'San Francisco',
-      destinationCities: [
-          { city: 'São Paulo', numberOfDays: 4 },
-          { city: 'Rio de Janeiro', numberOfDays: 3 }
-      ],
-      numberOfPeople: 1,
-    },
-  ];
+import { useTrips } from '../../context/TripsContext.js';
+import { tripTitle, tripDateString } from '../../utils';
 
 function Home() {
+  const { trips, } = useTrips();
+
   return (
       <div className="page home">
         <h1>My trips</h1>
@@ -38,9 +16,9 @@ function Home() {
 
           <div className="trips-container">
             {trips.map((trip, index) =>
-              <Link to="/trip">
+              <Link to={`/trip/${trip.id}`} key={`trip-${index}`}>
                 <div className="trip-card">
-                  <div className="trip-title">{trip.title}</div>
+                  <div className="trip-title">{tripTitle(trip)}</div>
                   <div className="trip-dates">{tripDateString(trip)}</div>
                 </div>
               </Link>
--- src/pages/Trip/index.js
+++ src/pages/Trip/index.js
@@ -1,10 +1,53 @@
 import '../../assets/app.css';
-import { tripDateString } from '../../utils';
+
+import { useTrips } from '../../context/TripsContext.js';
+import {
+  useParams
+} from 'react-router-dom';
+import {
+  tripTitle,
+  tripDateString,
+  destinationCitiesString,
+  numberOfPeopleString,
+  dayItineraryDate,
+  itineraryItemSummaryDescription,
+} from '../../utils';
 
 function Trip() {
+  const { tripId, } = useParams();
+  const { trips, } = useTrips();
+  
+  const trip = trips.find(({ id }) => id == tripId);
+
   return (
-      <div className="page trip">
-        <h1>TRIP</h1>
+      <div className="trip">
+        <div className="header">
+        { tripTitle(trip) }
+        </div>
+
+        <div className="page">
+          <div className="query-summary">
+            {destinationCitiesString(trip)} • {tripDateString(trip)} • {numberOfPeopleString(trip)}
+          </div>
+
+          <div className="overall-itinerary">
+            {trip.itinerary.map((dayItinerary, idx) =>
+              <div className="day-itinerary" key={`day-${idx}`}>
+                <div className="date">
+                {dayItineraryDate(trip, idx)}
+                </div>
+
+                <div className="day-itinerary-items">
+                  {dayItinerary.map((itineraryItem, idx2) =>
+                    <div className="day-itinerary-item" key={`day-${idx}-item-${idx2}`}>
+                      {itineraryItemSummaryDescription(itineraryItem)}
+                    </div>
+                  )}
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
   );
 }
